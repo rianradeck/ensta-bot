0be8b84fb354b83269b4cce33b8df542d486bc33
Rian Radeck <rianradeck3@gmail.com>
2024-09-20 18:14:06 +0200

    pre-commit run

--- src/net_info/net_info.py
+++ src/net_info/net_info.py
@@ -1,12 +1,6 @@
-import asyncio
-import copy
-import os
-from pathlib import Path
-from threading import Thread
-
 import subprocess
-import discord
-import pytubefix
+from pathlib import Path
+
 from discord.ext import commands
 
 from utils import call_admin
@@ -26,23 +20,31 @@ def load_net_info(_client: commands.Bot):
 def load_commands():
     client.command()(ip)
 
+
 async def ip(ctx: commands.Context, *args):
     subcommand, *query = args
 
     match subcommand:
         case "info":
-            ipconfig = (subprocess.Popen(["ipconfig"], stdout=subprocess.PIPE ).communicate()[0]).decode("utf-8")
-            info_start = ipconfig.find("Wi-Fi")
-            info_end = info_start
-            n_collons = 0
-            while n_collons < 12:
-                if ipconfig[info_end] == ":":
-                    n_collons += 1
-                info_end += 1
-            infos = ipconfig[info_start:info_end].split("\n")
-            infos = "```" + "\n".join(infos[2:-1]) + "```"
-            infos = infos.replace(". ", "")
-            infos = infos.replace(" :", ":")
-            await ctx.send(infos)
+            try:
+                ipconfig = (
+                    subprocess.Popen(
+                        ["ipconfig"], stdout=subprocess.PIPE
+                    ).communicate()[0]
+                ).decode("utf-8")
+                info_start = ipconfig.find("Wi-Fi")
+                info_end = info_start
+                n_collons = 0
+                while n_collons < 12:
+                    if ipconfig[info_end] == ":":
+                        n_collons += 1
+                    info_end += 1
+                infos = ipconfig[info_start:info_end].split("\n")
+                infos = "```" + "\n".join(infos[2:-1]) + "```"
+                infos = infos.replace(". ", "")
+                infos = infos.replace(" :", ":")
+                await ctx.send(infos)
+            except Exception as e:
+                await call_admin(ctx, e)
         case _:
             await ctx.send("Invalid subcommand.")
